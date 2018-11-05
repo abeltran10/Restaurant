7180907a5dfdf9c382e1e10b6a9e47e6602e3dff
abeltran10 <albert.beltran.mar@gmail.com>
2018-11-05 19:28:47 +0100

    Prueba

---
+++ app/src/main/java/com/abeltran/restaurant/app/restaurant/vista/MainActivity.java
@@ -0,0 +1,22 @@
+package com.abeltran.restaurant.app.restaurant.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.abeltran.restaurant.app.restaurant.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+
+}
